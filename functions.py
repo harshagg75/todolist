37c198cf927d8275e99732670b3a6a958ac67fbc
harsh aggarwal <harshagg75@gmail.com>
2023-03-11 09:22:09 +0530

    add filepath

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-def get_todos(filepath):
+filepath = todo.txt
+def get_todos(filepath=todo.txt):
     with open(filepath,"r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
